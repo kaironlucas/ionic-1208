5e2863694e0dc0093c9450a7b1d346fbc8634d5a
kaironlucas <noriakxp@gmail.com>
2019-08-12 20:08:51 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-1208
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-1208)
---
+++ pages/home/home.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  altura:number;
+  peso:number;
+  imc:number;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+  calc(){
+      this.imc = Number(this.peso) / (Number(this.altura) * Number(this.altura));
+
+    
+  }
+
+}
